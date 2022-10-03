30abedbdc3dae388ddec1a95bb5ca282bb4ac1e7
Fagbenja Enoch <fagbenjaenoch73@gmail.com>
2022-10-03 23:57:13 +0100

    update index.js

--- Solo Project  7 - Color Scheme Generator/js/index.js
+++ Solo Project  7 - Color Scheme Generator/js/index.js
@@ -4,6 +4,21 @@ const modeInput = document.querySelector('#mode')
 const colorsEl = document.querySelector('.colors')
 const form = document.querySelector('#color-form')
 
+fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${mode}&count=5&format=json`)
+    .then(res => res.json())
+    .then(result => {
+      //alert(result)
+      const { colors } = result
+      colorsEl.innerHTML = colors.map(({ hex }) => {
+        `
+          <div class="color-card">
+	          <div class="color-display" style="${hex}"></div>
+	          <p class="color-code">${hex}</p>
+	      </div>
+        `
+      }).join('')
+    })
+
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   const seedColor = seedColorInput.value
